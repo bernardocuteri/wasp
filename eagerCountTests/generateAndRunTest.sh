754eddb530180ddfbc08eb6d091a4c14d7ca06d5
Bernardo Cuteri <bernardo.cuteri@gmail.com>
2020-01-16 17:20:29 +0100

    eager count tests fix

--- eagerCountTests/generateAndRunTest.sh
+++ eagerCountTests/generateAndRunTest.sh
@@ -1,9 +1,9 @@
-#numberOfVariables numberOfRules numberOfPredicates maxArity maxBodySize maxHeadSize nFacts maxConstant constantRatio
-#./generateBenchmark.py 2 3 3 2 2 2 10 10
+#numberOfVariables numberOfRules numberOfPredicates maxArity maxBodySize maxHeadSize nFacts maxConstant constantRatio maxArithmeticExpressionsPerRule
+#./generateBenchmark.py 2 3 3 2 2 2 10 10 5 2
 #4 4 4 3 3 2 20 10
 count=0
 until [[ $variable =~ FAILED ]]; do
-        ./generateBenchmark.py 3 5 3 3 4 3 10 10 3
+        ./generateBenchmark.py 3 5 3 3 4 3 10 10 4 2
        variable=`./runTest.py`
        count=$((count+1))
        echo ${variable}
